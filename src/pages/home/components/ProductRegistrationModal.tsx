53d21af34348b3b9502555575ef1985ee52c2d72
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2024-10-10 16:30:09 +0900

    [refactor] React-Hook-Form 적용

--- src/pages/home/components/ProductRegistrationModal.tsx
+++ src/pages/home/components/ProductRegistrationModal.tsx
@@ -17,11 +17,12 @@ import {
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { ALL_CATEGORY_ID, categories } from '@/constants';
-import { createNewProduct, initialProductState } from '@/helpers/product';
+import { createNewProduct } from '@/helpers/product';
 import { useProductStore } from '@/store/product/productStore';
 import { useToastStore } from '@/store/toast/toastStore';
 import { uploadImage } from '@/utils/imageUpload';
 import { ChangeEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 interface ProductRegistrationModalProps {
   isOpen: boolean;
@@ -32,38 +33,44 @@ interface ProductRegistrationModalProps {
 export const ProductRegistrationModal: React.FC<
   ProductRegistrationModalProps
 > = ({ isOpen, onClose, onProductAdded }) => {
-  const [product, setProduct] = useState<NewProductDTO>(initialProductState);
   const showToast = useToastStore((state) => state.showToast);
   const { addProduct } = useProductStore();
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void => {
-    const { name, value } = e.target;
-    setProduct((prev) => ({ ...prev, [name]: value }));
-  };
+  const [image, setImage] = useState<File | null>(null);
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    reset,
+    formState: { errors },
+  } = useForm<NewProductDTO>(); // NewProductDTO 타입을 폼 데이터로 사용
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const files = e.target.files;
     if (files && files.length > 0) {
-      const file = files[0];
-      setProduct((prev) => ({ ...prev, image: file }));
+      setImage(files[0]);
     }
   };
 
-  const handleSubmit = async (): Promise<void> => {
+  const onSubmit = async (data: NewProductDTO): Promise<void> => {
     try {
-      if (!product.image) {
+      if (!image) {
         throw new Error('이미지를 선택해야 합니다.');
       }
 
-      const imageUrl = await uploadImage(product.image as File);
+      const imageUrl = await uploadImage(image as File);
       if (!imageUrl) {
         throw new Error('이미지 업로드에 실패했습니다.');
       }
 
-      const newProduct = createNewProduct(product, imageUrl);
+      const newProduct = createNewProduct({ ...data }, imageUrl);
       await addProduct(newProduct);
       showToast('성공적으로 물품을 등록했습니다');
+
+      // form 이미지 초기화
+      reset();
+      setImage(null);
       onClose();
       onProductAdded();
     } catch (error) {
@@ -72,10 +79,12 @@ export const ProductRegistrationModal: React.FC<
   };
 
   const handleCategoryChange = (value: string): void => {
-    setProduct((prev) => ({
-      ...prev,
-      category: { ...prev.category, id: value },
-    }));
+    const selectedCategory = categories.find(
+      (category) => category.id === value
+    );
+    if (selectedCategory) {
+      setValue('category', selectedCategory);
+    }
   };
 
   return (
@@ -86,30 +95,30 @@ export const ProductRegistrationModal: React.FC<
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <Input
-            name="title"
+            {...register('title', { required: '상품명을 입력해야 합니다.' })}
             placeholder="상품명"
-            onChange={handleChange}
-            value={product.title || ''}
           />
+          {errors.title && (
+            <p style={{ color: 'red', fontWeight: 'bold' }}>
+              {errors.title.message as string}
+            </p>
+          )}
           <Input
-            name="price"
-            type="number"
+            {...register('price', { required: '상품 가격을 입력해야 합니다.' })}
             placeholder="가격"
-            onChange={handleChange}
-            value={product.price || ''}
+            type="number"
           />
+          {errors.price && (
+            <p style={{ color: 'red', fontWeight: 'bold' }}>
+              {errors.price.message as string}
+            </p>
+          )}
           <Textarea
-            name="description"
+            {...register('description')}
             className="resize-none"
             placeholder="상품 설명"
-            onChange={handleChange}
-            value={product.description || ''}
           />
-          <Select
-            name="categoryId"
-            onValueChange={handleCategoryChange}
-            value={product.category.id || ''}
-          >
+          <Select onValueChange={handleCategoryChange}>
             <SelectTrigger>
               <SelectValue placeholder="카테고리 선택" />
             </SelectTrigger>
@@ -131,7 +140,7 @@ export const ProductRegistrationModal: React.FC<
           />
         </div>
         <DialogFooter>
-          <Button onClick={handleSubmit}>등록</Button>
+          <Button onClick={handleSubmit(onSubmit)}>등록</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
